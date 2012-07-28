3488e9626795c0b89d2f26a55abd5735756b1e71
Thomas Profelt <protom@localhost.(none)>
2012-07-28 22:50:48 +0200

    Added test for index command

--- commands/index.js
+++ commands/index.js
@@ -20,33 +20,28 @@ function buildIndexKey(key, propName, propValue) {
 	return key + propName + "/" + propValue;
 }
 
-var commandMap = {
-	"create": {
-		"execute": { 
-			"index": createIndex
+var sampleCommand = {
+		name: "sample",
+		create: {
+			execute: function(a) {a+6},
+			rollback: function(b) {b+5},
+			validate: function(g) {g+7}
 		},
-		"rollback": {
-			"index": removeIndex
+		update: {
+			execute: function(c) {c+4},
+			rollback: function(d) {d+3},
+			validate: function(h) {h+8}
+		},
+		remove: {
+			execute: function(e) {e+2},
+			rollback: function(f) {f+1},
+			validate: function(i) {i+9}
 		}
-	},
+	}
 
-	"update": {
-		"execute": {
-			"index": updateIndex
-		},
-		"rollback": {
-			"index": rollbackIndexUpdate
-		}
-		
-	},
-
-	"remove": {
-		"execute": {
-			"index": removeIndex
-		},
-		"rollback": {
-			"index": createIndex
-		}
-		
+module.exports = {
+	name: "index",
+	create: {
+		execute: createIndex
 	}
 }
